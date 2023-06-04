04678e6d1ff349c5db59fb0cd439aaca4634c58c
T1murCoder <timur.zarutskiy@yandex.ru>
2023-06-04 20:40:05 +0300

    допиливаю

--- website/forms/CreateProjectForm.py
+++ website/forms/CreateProjectForm.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, Optional, NumberRange
 
 
-class RegisterForm(FlaskForm):
-    name = StringField('Имя пользователя', validators=[DataRequired()])
+class CreateProjectForm(FlaskForm):
+    name = StringField('Имя проекта', validators=[DataRequired()])
     max_members = IntegerField('Максимальное число участников:', validators=[DataRequired(), NumberRange(min=1)])
-    description =  TextAreaField('')
-    submit = SubmitField('Создатб')
+    description =  TextAreaField('Введите описание:', validators=[Optional()])
+    submit = SubmitField('Создать')
--- website/views.py
+++ website/views.py
@@ -3,23 +3,43 @@ from flask_login import login_required, current_user
 from data import db_session
 from data.users import User
 from data.projects import Project
+from .forms.CreateProjectForm import CreateProjectForm
 
 
-views = Blueprint("views", __name__, template_folder="../templates", static_url_path="../static")
+views = Blueprint("views", __name__, template_folder="../templates")
 
 
 @views.route("/")
 @views.route("/home")
 def home():
-    
     db_sess = db_session.create_session()
     projects = db_sess.query(Project).all()
 
     return render_template("home.html", title='Home', projects=projects, user=current_user)
 
-
+@views.route("/create-project", methods=['GET', 'POST'])
+@login_required
 def create_project():
-    pass
+    form = CreateProjectForm()
+    
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).get(current_user.id)
+        
+        project = Project(
+            name=form.name.data,
+            max_members=form.max_members.data,
+            description=form.description.data
+        )
+        project.set_leader(user)
+        
+        db_sess.add(project)
+        db_sess.commit()
+        
+        return redirect(url_for("views.home"))
+    
+    return render_template("create_project.html", form=form, user=current_user)    
+
 
 @views.route("/join-project/<int:project_id>")
 @login_required
@@ -28,7 +48,7 @@ def join_project(project_id):
     project = db_sess.query(Project).get(project_id)
     user = db_sess.query(User).get(current_user.id)
     
-    if user in project.members:
+    if user in project.members or len(project.members) == project.max_members:
         abort(404)
     
     project.add_member(user)
@@ -39,3 +59,16 @@ def join_project(project_id):
 
 def leave_project():
     pass
+
+
+def delete_project():
+    pass
+
+
+@views.route("/my-projects")
+@login_required
+def my_projects():
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).get(current_user.id)
+    
+    return render_template("my_projects.html", projects=user.projects, user=current_user)
